6f777d17f21b81efcb736699a571fe4b44a00694
Mike Schon <gnarlyman@proton.me>
2025-07-31 20:28:30 -0600

    fixed issues

---
+++ static/js/websocket.js
@@ -0,0 +1,11 @@
+const ws = new WebSocket("ws://" + window.location.host + "/ws");
+ws.onmessage = function(event) {
+    if (event.data === "new-room") {
+        htmx.trigger("#rooms-list", "new-room");
+    } else if (event.data === "new-chat") {
+        htmx.trigger("#messages-list", "new-chat");
+    }
+};
+ws.onclose = function() {
+    setTimeout(() => location.reload(), 1000);
+};
