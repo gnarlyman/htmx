ad5ce39eb980c128c8fd50da3e0dc86b59535202
gnarlyman <gnarlyman@proton.me>
2025-08-05 03:28:39 -0600

    fixed spacing between chats

--- internal/components/partials/RoomContent_templ.go
+++ internal/components/partials/RoomContent_templ.go
@@ -57,7 +57,7 @@ func RoomContent(room *models.Room, chats []*models.Chat) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" hx-trigger=\"new-chat from:body\" hx-swap=\"innerHTML\" class=\"space-y-4\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" hx-trigger=\"new-chat from:body\" hx-swap=\"innerHTML\" class=\"space-y-2\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
