cc0d7860490be26f4ccddbba7967c727c2bd04f0
gnarlyman <gnarlyman@proton.me>
2025-08-04 20:04:27 -0600

    don't use air proxy

--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,11 +1,12 @@
 const ws = new WebSocket("ws://" + window.location.host + "/ws");
-ws.onmessage = function(event) {
+ws.onmessage = function (event) {
     if (event.data === "new-room") {
         htmx.trigger("#rooms-list", "new-room");
     } else if (event.data === "new-chat") {
         htmx.trigger("#messages-list", "new-chat");
     }
 };
-ws.onclose = function() {
+ws.onclose = function () {
     setTimeout(() => location.reload(), 1000);
+    console.error("WebSocket connection closed. Reconnecting...");
 };
